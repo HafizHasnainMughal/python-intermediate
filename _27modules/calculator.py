fded694ab3b782b730f380eb1f0519fe97cdb960
hafiz hasnain mughal <hafizmuhammadhasnain12@gmail.com>
2026-08-23 11:38:28 +0500

    firs init

---
+++ _27modules/calculator.py
@@ -0,0 +1,10 @@
+def addition(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
---
+++ _27modules/modules.py
@@ -0,0 +1,27 @@
+# modules in python 
+'''
+    Python module ek .py file hoti hai jisme Python code,
+    functions, classes, ya variables hote hain, 
+    jise hum doosri Python file mein reuse kar sakte hain.
+
+    There are three types of modules in python
+    1.Built-in / Standard Library Modules
+        wo modules ya libraries jo python povide karta hay
+        like >>math, random, os, datetime, statistics, json, re etc
+    2.User-defined Modules
+        Jo module hum khud banate hain.
+        like calulator.py
+    3. Third-party Modules
+        Jo Python ke saath default nahi aate aur usually pip se install kiye jate hain.
+        like pandas, numpy, matplotlib etc 
+
+'''
+print("-----------------------")
+from calculator import addition,subtract,multiply
+
+reult=addition(10, 5)
+print(reult)
+print(subtract(10, 5))
+print(multiply(10, 5))
+
+print("-----------------------")
